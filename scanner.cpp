ecf45a9c04fc34bd56d4c3ca9263496846afe0b2
cole.cathcart1 <cole.cathcart1@cs.ucalgary.ca>
2023-02-03 13:48:28 -0700

    Added more scanner logic Added most of the logic for the scanner, including handling for multi-char tokens, strings, integers, ID's, and keywords

--- scanner.cpp
+++ scanner.cpp
@@ -12,6 +12,7 @@
 #include "token.hpp"
 #include "scanner.hpp"
 #include "logger.hpp"
+#include <ctype.h>
 
 //Default constructor
 //
@@ -35,10 +36,13 @@ bool iswhitespace(char ch){
 
 //helper function to check for non-ascii characters
 bool isnotascii(char ch){
+    if(ch > 127){
+        return true;
+    }
     return false;
 }
 
-//checks for operators and punctuation that are not 
+//helper function that checks for operators and punctuation that are not 
 //potentially part of a multi-char operator
 bool issingleton(char ch){
     if(ch == '+' ||
@@ -51,6 +55,36 @@ bool issingleton(char ch){
     ch == ')' ||
     ch == '{' ||
     ch == '}') {
+        return true;
+    }
+    return false;
+}
+
+//helper function that checks if a given string
+//matches one of the keywords as defined by the GoLF specs
+bool iskeyword(std::string word){
+    if(word == "if" ||
+    word == "for" ||
+    word == "else" ||
+    word == "var" ||
+    word == "func" ||
+    word == "break" ||
+    word == "return"){
+        return true;
+    }
+    return false;
+}
+
+//helper function that checks if the character following a '\'
+//in a string is legal as per the GoLF specs ('\' is not included when using)
+bool islegalescape(char ch){
+    if(ch == 'b' ||
+    ch == 'f' ||
+    ch == 'n' ||
+    ch == 'r' ||
+    ch == 't' ||
+    ch == '\\' ||
+    ch == '"'){
         return true;
     }
     return false;
@@ -76,12 +110,12 @@ Token Scanner::lex(){
 
         //check for non-ascii characters
         }else if(isnotascii(ch)){
-            logger->warning("Skipping non-ASCII character");
+            logger->warning("Skipping non-ASCII character",line);
             continue;
 
         //check for ascii-NUL
         } else if(ch == '\0'){
-            logger->warning("Skipping NUL character");
+            logger->warning("Skipping NUL character",line);
             continue;
 
         //check for whitespace
@@ -142,7 +176,7 @@ Token Scanner::lex(){
             if(ch == '&'){
                 lastoken = Token("&&","&&",line);
             } else {
-                logger->error("Bitwise AND not supported in GoLF");
+                logger->error("Bitwise AND not supported in GoLF",line);
             }
         
         //check for '||'
@@ -151,7 +185,7 @@ Token Scanner::lex(){
             if(ch == '|'){
                 lastoken = Token("||","||",line);
             } else {
-                logger->error("Bitwise OR not supported in GoLF");
+                logger->error("Bitwise OR not supported in GoLF",line);
             }
         
         //check for '/' or comment (//...)
@@ -167,10 +201,71 @@ Token Scanner::lex(){
                 file.unget();
                 lastoken = Token("/","/",line);
             }
-        }
-    }
 
-    return lastoken;
+        //check for integers
+        } else if(isdigit(ch)){
+            std::string number = "";
+            while(isdigit(ch)){
+                number.push_back(ch);
+                ch = file.get();
+            }
+            file.unget();
+            lastoken = Token("INTEGER",number,line);
+        
+        //check for identifiers or keywords
+        } else if(isalpha(ch) || ch == '_'){
+            std::string word = "";
+            while(isalpha(ch) || ch == '_' || isdigit(ch)){
+                word.push_back(ch);
+                ch = file.get();
+            }
+            //error checking for special case that this is an unescaped double-quote in a string
+            if(ch == '"' && lastoken.type == "STRING"){
+                logger->error("Unescaped double-quote in string",line);
+            }
+            file.unget();
+            if(iskeyword(word)){
+                lastoken = Token("KEYWORD",word,line);
+            } else {
+                lastoken = Token("ID",word,line);
+            }
+            
+        //check for strings and string errors    
+        } else if(ch == '"'){
+            ch = file.get();
+            std::string string = "";
+            while(ch != '"'){
+                if(ch == EOF){
+                    logger->error("String terminated by end-of-file",line);
+                }
+                if(ch == '\n'){
+                    logger->error("String contains newline character",line);
+                }
+                if(ch == '\\'){
+                    char secondchar = file.get();
+                    if(!islegalescape(secondchar)){
+                        std::string badchar(1,ch);
+                        std::string escaperr = "Illegal escape character " + badchar;
+                        logger->error(escaperr);
+                    }
+                    string.push_back(ch);
+                    string.push_back(secondchar);
+                } else {
+                    string.push_back(ch);
+                    ch = file.get();
+                }
+            }
+            lastoken = Token("STRING",string,line);
+
+        //unrecognized characters
+        } else {
+            logger->warning("Skipping unrecognized character",line);
+            continue;
+        }
+        return lastoken;
+    }
+    //SHOULD NEVER BE REACHED
+    return Token("","",0);
 }
 
 //'Undoes' the previous lex (for use by the parser)
