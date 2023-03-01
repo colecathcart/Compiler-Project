183164c7f5feedb27b2bb9426fa4d09a556b0e18
cole.cathcart1 <cole.cathcart1@cs.ucalgary.ca>
2023-02-28 23:59:03 -0700

    Minor documentation

--- ast.cpp
+++ ast.cpp
@@ -1,3 +1,12 @@
+/// CPSC 411 Term Project: Main Class
+/// Cole Cathcart, 30043556
+/// Feb 28, 2023
+/// Class to represent and contain information about an AST.
+/// Based loosely off Dr. Aycock's example code 
+/// in python, as shown in the video series "Calculator demo video 
+/// series" under "Other resources" on the CPSC 411 d2l page:
+/// https://youtu.be/NcIhZ_6l3T8 
+///
 #include "token.hpp"
 #include "ast.hpp"
 
